ca7ea25597dba1ed6471a491a7be22f600bfce6d
Jacqueline Lee <anginblu@gmail.com>
2017-12-28 22:53:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,9 +11,11 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
     elsif req.path.match(/cart/)
       if @@items.empty?
         resp.write "Your cart is empty\n"
@@ -22,6 +24,7 @@ class Application
           resp.write "#{item}\n"
         end
       end
+
     elsif req.path.match(/add/)
       search_term = req.params["q"]
       if @@items.include?(search_term)
@@ -35,6 +38,7 @@ class Application
     end
 
     resp.finish
+    
   end
 
   def handle_search(search_term)
